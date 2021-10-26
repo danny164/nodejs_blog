a731575e17986c49ef71ac5e8f42719e8fbb7b82
DANNY <qnv164@gmail.com>
2021-10-26 15:25:32 +0700

    Update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
+const coursesRouter = require('./courses');
+const meRouter = require('./me');
 
 // app.get('/news', (req, res) => {
 //     res.render('news');
@@ -7,6 +9,8 @@ const siteRouter = require('./site');
 
 function route(app) {
     app.use('/news', newsRouter);
+    app.use('/courses', coursesRouter);
+    app.use('/me', meRouter);
     app.use('/', siteRouter);
 }
 
