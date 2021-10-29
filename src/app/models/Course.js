056b3deb9ec6fb83baf787d8ceaa8b466465ac84
DANNY <qnv164@gmail.com>
2021-10-29 15:25:32 +0700

    Update

---
+++ src/app/models/Course.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
+
+const Schema = mongoose.Schema;
+
+const Course = new Schema(
+    {
+        name: { type: String, required: true },
+        des: { type: String },
+        image: { type: String },
+        videoId: { type: String },
+        slug: { type: String, slug: 'name', unique: true },
+        level: { type: String },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model('Course', Course);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
+const route = require('./routes');
+
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
 
 const app = express();
 const port = 3000;
-
-const route = require('./routes');
 
 // Static file
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,6 +23,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -26,15 +33,18 @@ app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     })
 );
 app.set('view engine', 'hbs');
 
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Route init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
