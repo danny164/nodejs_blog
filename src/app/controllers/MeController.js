f345f89e82279ccdf5620f0945b96e9aabc4b459
DANNY <qnv164@gmail.com>
2021-11-21 20:30:25 +0700

    Update

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,9 +4,30 @@ const { mutipleMongooseToObject } = require('../util/mongoose');
 class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then((courses) =>
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deleteCount]) => {
                 res.render('me/stored-courses', {
+                    deleteCount,
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
+
+        // ! Đã gộp lên phía trên
+        // Course.find({})
+        //     .then((courses) =>
+        //         res.render('me/stored-courses', {
+        //             courses: mutipleMongooseToObject(courses),
+        //         })
+        //     )
+        //     .catch(next);
+    }
+
+    // [GET] /me/trash/courses
+    trashCourses(req, res, next) {
+        Course.findDeleted({})
+            .then((courses) =>
+                res.render('me/trash-courses', {
                     courses: mutipleMongooseToObject(courses),
                 })
             )
